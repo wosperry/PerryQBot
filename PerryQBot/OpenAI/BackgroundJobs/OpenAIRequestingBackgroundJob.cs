d5c83daa92454003e99558beee835098bec66fd4
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-21 09:30:34 +0800

    1

--- PerryQBot/OpenAI/BackgroundJobs/OpenAIRequestingBackgroundJob.cs
+++ PerryQBot/OpenAI/BackgroundJobs/OpenAIRequestingBackgroundJob.cs
@@ -41,7 +41,7 @@ public class OpenAIRequestingBackgroundJob : BackgroundJob<OpenAIRequestingBackg
                 {
                     var messageChain = new MessageChainBuilder()
                         .At(args.SenderId)
-                        .Plain($" 大佬好\r\n{message}")
+                        .Plain($" 大佬，\r\n{message}")
                         .Build();
 
                     MessageManager.SendGroupMessageAsync(args.GroupId, messageChain).Wait();
