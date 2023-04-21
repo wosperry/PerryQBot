163b7c2e53276bbbdf75af0188731eb638ac1f06
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-21 22:09:24 +0800

    edit readme

--- PerryQBot/Commands/Handlers/HelpCommandHandler.cs
+++ PerryQBot/Commands/Handlers/HelpCommandHandler.cs
@@ -6,10 +6,10 @@ public class HelpCommandHandler : CommandHandlerBase
     public override string HandleAndResponseAsync(CommandContext context)
     {
         return """
-               以下是支持的命令写法，方括号内表示内容，方括号外是说明
-                   #帮助
-                   #修改预设 你的名字叫张三。
-                   #获取预设
-               """;
+            以下是支持的命令
+            #帮助
+            #修改预设 我想让你充当英英词典，对于给出的英文单词，你要给出其中文意思以及英文解释，并且给出一个例句，此外不要有其他反馈，第一个单词是“Hello"。
+            #获取预设
+            """;
     }
 }
