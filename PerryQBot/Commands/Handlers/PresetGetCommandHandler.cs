b2fb872a5d110333483f7d05c656d6be230dccd2
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-22 01:27:42 +0800

    查看预设

---
+++ PerryQBot/Commands/Handlers/PresetGetCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using PerryQBot.EntityFrameworkCore.Entities;
+using Volo.Abp.Domain.Repositories;
+
+namespace PerryQBot.Commands.Handlers;
+
+[Command("修改预设", "修改我的预设")]
+public class PresetGetCommandHandler : CommandHandlerBase
+{
+    public IRepository<User> UserRepository { get; set; }
+
+    public override async Task<string> HandleAndResponseAsync(CommandContext context)
+    {
+        var user = await UserRepository.FirstOrDefaultAsync(t => t.QQ == context.SenderId);
+        return $"您的预设为：{user?.Preset}";
+    }
+}
