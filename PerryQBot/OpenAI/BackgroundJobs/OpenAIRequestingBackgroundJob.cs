f4236621061b4730cc9d41515215baa9f6951341
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-21 10:33:19 +0800

    啊 好奇怪，@完怎么不加空格换行

--- PerryQBot/OpenAI/BackgroundJobs/OpenAIRequestingBackgroundJob.cs
+++ PerryQBot/OpenAI/BackgroundJobs/OpenAIRequestingBackgroundJob.cs
@@ -27,7 +27,7 @@ public class OpenAIRequestingBackgroundJob : BackgroundJob<OpenAIRequestingBackg
 
             var flurlResult = await url.PostAsync(JsonContent.Create(requestContent));
             var result = await flurlResult.GetJsonAsync<AiResponse>();
-            var message = " " + result.Choices.FirstOrDefault()?.Message?.Content?.Replace("\r\n\r\n", "\r\n") ?? "我不知道怎么回答你";
+            var message = "\r\n" + (result.Choices.FirstOrDefault()?.Message?.Content?.Replace("\r\n\r\n", "\r\n") ?? "我不知道怎么回答你");
 
             lock ("bot")
             {
