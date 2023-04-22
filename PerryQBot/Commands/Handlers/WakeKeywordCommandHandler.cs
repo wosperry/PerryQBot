a19542794a28bec0f4063398c35a05131ad52493
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-22 19:19:43 +0800

    1

--- PerryQBot/Commands/Handlers/WakeKeywordCommandHandler.cs
+++ PerryQBot/Commands/Handlers/WakeKeywordCommandHandler.cs
@@ -2,9 +2,9 @@
 
 namespace PerryQBot.Commands.Handlers
 {
-    [Command("ai", WithPrefix = false)]
-    [Command("喵", WithPrefix = false)]
-    [Command("呐", WithPrefix = false)]
+    [Command("ai", false)]
+    [Command("喵", false)]
+    [Command("呐", false)]
     [ExposeServices(typeof(ICommandHandler))]
     public class WakeKeywordCommandHandler : CommandHandlerBase
     {
