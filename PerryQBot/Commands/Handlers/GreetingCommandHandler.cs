c263e82bad83c20ad3406bb85598cb08cf9670a0
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-22 17:55:07 +0800

    自改readme

--- PerryQBot/Commands/Handlers/GreetingCommandHandler.cs
+++ PerryQBot/Commands/Handlers/GreetingCommandHandler.cs
@@ -1,16 +1,15 @@
 ﻿using Volo.Abp.DependencyInjection;
 
-namespace PerryQBot.Commands.Handlers
-{
-    [Command("你好")]
-    [ExposeServices(ICommandHandler)]
-    public class GreetingCommandHandler : CommandHandlerBase
-    {
-        public override async Task<string> HandleAndResponseAsync(CommandContext context)
-        {
-            await Task.CompletedTask;
+namespace PerryQBot.Commands.Handlers;
 
-            return $"你好，{context.SenderName}";
-        }
+[Command("你好")]
+[ExposeServices(typeof(ICommandHandler))]
+public class GreetingCommandHandler : CommandHandlerBase
+{
+    public override async Task<string> HandleAndResponseAsync(CommandContext context)
+    {
+        await Task.CompletedTask;
+
+        return $"你好，{context.SenderName}";
     }
 }
