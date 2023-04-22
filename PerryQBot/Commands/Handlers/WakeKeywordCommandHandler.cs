bb8793ecdb0596ce518ccd3614d181d6c2a43feb
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-22 19:19:24 +0800

    添加唤醒词

--- PerryQBot/Commands/Handlers/WakeKeywordCommandHandler.cs
+++ PerryQBot/Commands/Handlers/WakeKeywordCommandHandler.cs
@@ -3,6 +3,8 @@
 namespace PerryQBot.Commands.Handlers
 {
     [Command("ai", WithPrefix = false)]
+    [Command("喵", WithPrefix = false)]
+    [Command("呐", WithPrefix = false)]
     [ExposeServices(typeof(ICommandHandler))]
     public class WakeKeywordCommandHandler : CommandHandlerBase
     {
