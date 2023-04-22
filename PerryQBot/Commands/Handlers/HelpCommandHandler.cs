baa6c4682bfbe43b8ab9f868e3d4ca840d8ca5be
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-22 17:58:04 +0800

    4

--- PerryQBot/Commands/Handlers/HelpCommandHandler.cs
+++ PerryQBot/Commands/Handlers/HelpCommandHandler.cs
@@ -25,7 +25,8 @@ public class HelpCommandHandler : CommandHandlerBase
                 3. {cmd}清空历史
                    - 说明：清空历史记录（不包含预设）
 
-                介绍：PerryQBot是一个使用Mirai.Net类库对接Mirai的QQ机器人，可以自动处理QQ消息并回复。该项目旨在管理QQ用户或群聊中@机器人的人的消息，每个用户都有一个预设，默认为空。
+                介绍：
+                PerryQBot是一个使用Mirai.Net类库对接Mirai的QQ机器人，可以自动处理QQ消息并回复。该项目旨在管理QQ用户或群聊中@机器人的人的消息，每个用户都有一个预设，默认为空。
                 聊天中可以保持数条历史记录以实现连续对话，智能对话是通过 GPT-3.5-turbo 的接口实现的。
                 该项目已在GitHub上公开，同时也在Gitee有开放的仓库，开源协议为AGPL3.0。如果您希望查看或贡献该项目，请点击以下链接访问：
                 https://github.com/wosperry/PerryQBot
