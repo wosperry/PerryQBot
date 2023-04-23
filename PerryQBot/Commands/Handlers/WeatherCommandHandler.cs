44d9202df6bdad3033b92f25a62ed64c1fddf219
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-24 02:42:09 +0800

    修改为爬取dom数据

--- PerryQBot/Commands/Handlers/WeatherCommandHandler.cs
+++ PerryQBot/Commands/Handlers/WeatherCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Flurl;
 using Flurl.Http;
+using HtmlAgilityPack;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using PerryQBot.Options;
@@ -32,15 +33,67 @@ namespace PerryQBot.Commands.Handlers
                 return;
             }
 
-            var nowUrl = new Url($"https://weather.cma.cn/api/now/{cityId}");
-            var now = await nowUrl.GetStringAsync();
+            var web = new HtmlWeb();
+            var doc = web.Load($"https://weather.cma.cn/web/weather/{cityId}.html");
+
+            // 获取一周天气数据
+            var weekNodes = doc.DocumentNode.SelectNodes("//div[contains(@class, 'pull-left day')]");
+            var weekData = weekNodes.Select(item =>
+            {
+                dynamic dynamicObject = new System.Dynamic.ExpandoObject();
+                dynamicObject.Day = item.SelectSingleNode(".//div[contains(@class, 'day-item')][1]").InnerText.Trim();
+                dynamicObject.Date = item.SelectSingleNode(".//div[contains(@class, 'day-item')][1]/br").NextSibling.InnerText.Trim();
+                dynamicObject.DayIcon = item.SelectSingleNode(".//div[contains(@class, 'dayicon')]/img").GetAttributeValue("src", "");
+                dynamicObject.DayWeather = item.SelectSingleNode(".//div[contains(@class, 'day-item')][3]").InnerText.Trim();
+                dynamicObject.DayWindDirection = item.SelectSingleNode(".//div[contains(@class, 'day-item')][4]").InnerText.Trim();
+                dynamicObject.DayWindPower = item.SelectSingleNode(".//div[contains(@class, 'day-item')][5]").InnerText.Trim();
+                dynamicObject.HighTemperature = item.SelectSingleNode(".//div[contains(@class, 'high')]").InnerText.Trim();
+                dynamicObject.LowTemperature = item.SelectSingleNode(".//div[contains(@class, 'low')]").InnerText.Trim();
+                dynamicObject.NightIcon = item.SelectSingleNode(".//div[contains(@class, 'nighticon')]/img").GetAttributeValue("src", "");
+                dynamicObject.NightWeather = item.SelectSingleNode(".//div[contains(@class, 'day-item')][8]").InnerText.Trim();
+                dynamicObject.NightWindDirection = item.SelectSingleNode(".//div[contains(@class, 'day-item')][9]").InnerText.Trim();
+                dynamicObject.NightWindPower = item.SelectSingleNode(".//div[contains(@class, 'day-item')][10]").InnerText.Trim();
+                return dynamicObject;
+            }).ToList();
+
+            // 获取当天详细数据
+            // 获取当天详细数据
+            var dayNodes = doc.DocumentNode.SelectSingleNode("//table[@class='hour-table' and @id='hourTable_0']");
+            var dayData = new List<dynamic>();
+            if (dayNodes != null)
+            {
+                var hourNodes = dayNodes.SelectNodes(".//tr[position() > 2]");
+                if (hourNodes != null)
+                {
+                    foreach (var hourNode in hourNodes)
+                    {
+                        dynamic hourData = new System.Dynamic.ExpandoObject();
+                        hourData.Time = hourNode.SelectSingleNode("./td[1]").InnerText.Trim();
+                        hourData.Temperature = hourNode.SelectSingleNode("./td[2]").InnerText.Trim();
+                        hourData.Weather = hourNode.SelectSingleNode("./td[3]").InnerText.Trim();
+                        hourData.Rain = hourNode.SelectSingleNode("./td[4]").InnerText.Trim();
+                        hourData.WindSpeed = hourNode.SelectSingleNode("./td[5]").InnerText.Trim();
+                        hourData.WindDirection = hourNode.SelectSingleNode("./td[6]").InnerText.Trim();
+                        hourData.Pressure = hourNode.SelectSingleNode("./td[7]").InnerText.Trim();
+                        hourData.Humidity = hourNode.SelectSingleNode("./td[8]").InnerText.Trim();
+                        hourData.Cloud = hourNode.SelectSingleNode("./td[9]").InnerText.Trim();
+                        dayData.Add(hourData);
+                    }
+                }
+            }
+
+            // 组装最终的动态对象
+            dynamic result = new System.Dynamic.ExpandoObject();
+            result.WeekData = weekData;
+            result.DayData = dayData;
+
+            var str = JsonConvert.SerializeObject(result);
 
             IsContinueAfterHandled = true;
             RequestMessage = $"""
-                城市是接口返回中的location
-                {now}
+                {str}
                 ---------
-                加上你对天气的分析，不允许胡编乱造假数据。记住一定要加上数据来源于中国气象局网站weather.cma.cn
+                分析以上数据，不允许胡编乱造假数据。记住一定要加上数据来源于中国气象局网站weather.cma.cn
                 此回复用于群聊，尽可能不要换行两次，而且不应用markdown格式
                 """;
         }
