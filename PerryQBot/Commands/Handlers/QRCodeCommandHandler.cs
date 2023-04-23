b6e8cfda984ba18c2c2b6692975ced8cef47296a
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-23 17:20:34 +0800

    添加聚合数据下载的二维码服务

--- PerryQBot/Commands/Handlers/QRCodeCommandHandler.cs
+++ PerryQBot/Commands/Handlers/QRCodeCommandHandler.cs
@@ -1,6 +1,14 @@
-﻿namespace PerryQBot.Commands.Handlers
+﻿using Volo.Abp.DependencyInjection;
+
+namespace PerryQBot.Commands.Handlers;
+
+[Command("qrcode")]
+[Command("二维码")]
+[ExposeServices(typeof(ICommandHandler))]
+public class QRCodeCommandHandler : CommandHandlerBase
 {
-    public class QRCodeCommandHandler
+    public override async Task ExecuteAsync(CommandContext context)
     {
+        await Task.CompletedTask;
     }
 }
