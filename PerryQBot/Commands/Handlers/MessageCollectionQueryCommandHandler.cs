eb59bbc32a05f473bc69d75a67ea86bd5511557c
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-23 13:48:18 +0800

    查询收藏结果分页

--- PerryQBot/Commands/Handlers/MessageCollectionQueryCommandHandler.cs
+++ PerryQBot/Commands/Handlers/MessageCollectionQueryCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Text.RegularExpressions;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using PerryQBot.EntityFrameworkCore.Entities;
 using PerryQBot.Options;
@@ -16,21 +17,37 @@ public class MessageCollectionQueryCommandHandler : CommandHandlerBase
 
     public override async Task ExecuteAsync(CommandContext context)
     {
+        var message = context.Message;
+        var page = 1;
+        var pageMatch = Regex.Match(message, "第([0-9]{1,3})页");
+        if (pageMatch.Success)
+        {
+            int.TryParse(pageMatch.Groups[1].ToString(), out page);
+        }
+
         var result = await (await DialogCollectionRepository.GetQueryableAsync())
-            .Where(t => t.Message.Contains(context.Message)
-                || t.QuoteMessage.Contains(context.Message))
+            .Where(t => t.Message.Contains(message)
+                || t.QuoteMessage.Contains(message))
+            .OrderBy(x => x.DateTime)
+            .Skip(MessageCollectionOptions.Value.MaxResultCount * (page - 1))
             .Take(MessageCollectionOptions.Value.MaxResultCount)
             .ToListAsync();
 
-        ResponseMessage = "OK，这是您要的结果：\r\n" + string.Join("-------------------", result.Select(x =>
+        if (result.Count > 0)
         {
-            return $"""
+            ResponseMessage = "OK，这是您要的结果：\r\n" + string.Join("-------------------",
+                result.Select(x =>
+                    $"""
 
-            {x.UserName}({x.UserQQ}) 收藏于 [{x.DateTime:yyyy-MM-dd HH:mm:ss}]
-            内容：{x.Message}
-            引用：{x.QuoteMessage}
+                    {x.UserName}({x.UserQQ}) 收藏于 [{x.DateTime:yyyy-MM-dd HH:mm:ss}]
+                    内容：{x.Message}
+                    引用：{x.QuoteMessage}
 
-            """;
-        }));
+                    """));
+        }
+        else
+        {
+            ResponseMessage = "没有找到您要的结果呢";
+        }
     }
 }
