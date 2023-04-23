8102766dac1cc4531288bfecf0bd62d7de9a460c
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-23 20:42:14 +0800

    readme

--- PerryQBot/Commands/Handlers/DiceCommandHandler.cs
+++ PerryQBot/Commands/Handlers/DiceCommandHandler.cs
@@ -1,12 +1,13 @@
-﻿using Mirai.Net.Utils.Scaffolds;
+﻿using Mirai.Net.Sessions.Http.Managers;
+using Mirai.Net.Utils.Scaffolds;
 using Volo.Abp.DependencyInjection;
 
 namespace PerryQBot.Commands.Handlers;
 
 [Command("骰子")]
 [ExposeServices(typeof(ICommandHandler))]
-public class DiceCommandHandler:CommandHandlerBase
-{  
+public class DiceCommandHandler : CommandHandlerBase
+{
     public override MessageChainBuilder OnMessageChainBuilding(MessageChainBuilder builder)
     {
         var number = new Random(DateTime.Now.Millisecond).Next(1, 6);
