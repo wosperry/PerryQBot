2602764a24d2f6b3503d25f68f7188d7e20aa866
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-24 21:59:51 +0800

    减少访问解析二维码频率

--- PerryQBot/CommandHandlers/QRDecodeCommandHandler.cs
+++ PerryQBot/CommandHandlers/QRDecodeCommandHandler.cs
@@ -2,8 +2,11 @@
 using Flurl.Http;
 using Microsoft.Extensions.Options;
 using Mirai.Net.Data.Messages.Concretes;
+using Mirai.Net.Data.Messages.Receivers;
+using Mirai.Net.Sessions.Http.Managers;
 using Mirai.Net.Utils.Scaffolds;
 using PerryQBot.Commands;
+using PerryQBot.EntityFrameworkCore.Entities;
 using PerryQBot.Options;
 
 namespace PerryQBot.CommandHandlers;
@@ -13,41 +16,50 @@ namespace PerryQBot.CommandHandlers;
 public class QRDecodeCommandHandler : CommandHandlerBase
 {
     public IOptions<Apis> Apis { get; set; }
-    public List<ImageMessage> ImageMessages { get; set; } = new();
+    public ImageMessage ImageMessage { get; set; }
 
     public override async Task ExecuteAsync(CommandContext context)
     {
         await Task.CompletedTask;
-        foreach (var m in context.MessageChain)
+
+        // 优先处理引用消息
+        var quoteMessage = context.MessageChain.FirstOrDefault(t => t is QuoteMessage) as QuoteMessage;
+        if (quoteMessage is not null)
         {
-            if (m is ImageMessage imageMessage)
+            var quoteMessageReceiver = await MessageManager.GetMessageReceiverByIdAsync<GroupMessageReceiver>(quoteMessage.MessageId, context.GroupId);
+            if (quoteMessageReceiver is not null)
             {
-                ImageMessages.Add(imageMessage);
+                // TODO: 有个问题，如果是其他类型消息怎么处理，比如图片，语音，表情等
+                ImageMessage = quoteMessageReceiver.MessageChain.FirstOrDefault(t => t is ImageMessage) as ImageMessage;
+                if (ImageMessage is not null)
+                {
+                    return;
+                }
             }
         }
+
+        // 处理图片消息
+        ImageMessage = context.MessageChain.FirstOrDefault(t => t is ImageMessage) as ImageMessage;
     }
 
     public override MessageChainBuilder OnMessageChainBuilding(MessageChainBuilder builder)
     {
-        if (ImageMessages.Count > 0)
-        {
-            foreach (var imageMessage in ImageMessages)
-            {
-                // 调接口解析
-                var url = new Url($"{Apis.Value.UomgAPI}/qr.encode")
-                    .SetQueryParam("url", imageMessage.Url);
+        if (ImageMessage is null)
 
-                var result = url.GetJsonAsync().Result;
-                if (result.code == 1)
-                {
-                    // 放上解析后的字符串
-                    builder.Plain($"内容：{result.qrurl}");
-                }
-            }
+        {
+            ResponseMessage = "请发送图片";
         }
         else
         {
-            ResponseMessage = "请发送图片";
+            // 调接口解析
+            var url = new Url($"{Apis.Value.UomgAPI}/qr.encode")
+                .SetQueryParam("url", ImageMessage.Url);
+            var result = url.GetJsonAsync().Result;
+            if (result.code == 1)
+            {
+                // 放上解析后的字符串
+                builder.Plain($"内容：{result.qrurl}");
+            }
         }
 
         return builder;
