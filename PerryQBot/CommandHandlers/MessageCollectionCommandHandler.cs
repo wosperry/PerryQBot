b368ac9398df29072bc114ff915577f1074a0ac3
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-24 22:07:41 +0800

    支持私聊里收藏消息

--- PerryQBot/CommandHandlers/MessageCollectionCommandHandler.cs
+++ PerryQBot/CommandHandlers/MessageCollectionCommandHandler.cs
@@ -18,38 +18,35 @@ public class MessageCollectionCommandHandler : CommandHandlerBase
 
     public override async Task ExecuteAsync(CommandContext context)
     {
-        // 只处理群聊的消息
-        if (context.Type == MessageReceivers.Group)
+        var dialogCollection = new DialogCollection
         {
-            var dialogCollection = new DialogCollection
-            {
-                Message = context.Message,
-                UserQQ = context.SenderId,
-                UserName = context.SenderName,
-                DateTime = DateTime.Now,
-            };
+            Message = context.Message,
+            UserQQ = context.SenderId,
+            UserName = context.SenderName,
+            DateTime = DateTime.Now,
+        };
 
-            // 处理引用消息
-            var quoteMessage = context.MessageChain.FirstOrDefault(t => t is QuoteMessage) as QuoteMessage;
-            if (quoteMessage is not null)
+        // 处理引用消息
+        var quoteMessage = context.MessageChain.FirstOrDefault(t => t is QuoteMessage) as QuoteMessage;
+        if (quoteMessage is not null)
+        {
+            var quoteMessageReceiver = await MessageManager.GetMessageReceiverByIdAsync<MessageReceiverBase>(quoteMessage.MessageId,
+                context.Type == MessageReceivers.Friend ? context.SenderId : context.GroupId);
+            if (quoteMessageReceiver is not null)
             {
-                var quoteMessageReceiver = await MessageManager.GetMessageReceiverByIdAsync<GroupMessageReceiver>(quoteMessage.MessageId, context.GroupId);
-                if (quoteMessageReceiver is not null)
+                // TODO: 有个问题，如果是其他类型消息怎么处理，比如图片，语音，表情等
+                var groupPlainMessage = quoteMessageReceiver.MessageChain.GetPlainMessage();
+                if (!string.IsNullOrWhiteSpace(groupPlainMessage))
                 {
-                    // TODO: 有个问题，如果是其他类型消息怎么处理，比如图片，语音，表情等
-                    var groupPlainMessage = quoteMessageReceiver.MessageChain.GetPlainMessage();
-                    if (!string.IsNullOrWhiteSpace(groupPlainMessage))
-                    {
-                        dialogCollection.QuoteMessage = groupPlainMessage;
-                    }
+                    dialogCollection.QuoteMessage = groupPlainMessage;
                 }
             }
-
-            // 保存消息
-            await DialogCollectionRepository.InsertAsync(dialogCollection);
-            // 设置回复消息
-            ResponseMessage = "收藏成功";
-            Logger.LogInformation("收藏成功：{obj}", JsonConvert.SerializeObject(dialogCollection));
         }
+
+        // 保存消息
+        await DialogCollectionRepository.InsertAsync(dialogCollection);
+        // 设置回复消息
+        ResponseMessage = "收藏成功";
+        Logger.LogInformation("收藏成功：{obj}", JsonConvert.SerializeObject(dialogCollection));
     }
 }
