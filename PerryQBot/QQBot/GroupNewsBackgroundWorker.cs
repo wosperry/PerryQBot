196207a205ea48bf70520ab2a552667e082cdefe
wosperry <55421194+wosperry@users.noreply.github.com>
2023-04-25 17:28:47 +0800

    1

---
+++ PerryQBot/QQBot/GroupNewsBackgroundWorker.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Mirai.Net.Sessions.Http.Managers;
+using Quartz;
+using Volo.Abp.BackgroundWorkers.Quartz;
+
+namespace PerryQBot.QQBot
+{
+    public class GroupNewsBackgroundWorker : QuartzBackgroundWorkerBase
+    {
+        public GroupNewsBackgroundWorker()
+        {
+            Trigger = TriggerBuilder.Create().WithIdentity(nameof(GroupNewsBackgroundWorker))
+                .WithCronSchedule("0 30 17 ? * *")
+                .StartNow().Build();
+        }
+
+        public override async Task Execute(IJobExecutionContext context)
+        {
+            await MessageManager.SendFriendMessageAsync("593281239", "五点半");
+        }
+    }
+}
